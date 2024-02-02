ffb8048003d9a7f7134c28227dcea9389f42dfa5
Jehad Almaliki <yourgitemail@example.com>
2024-02-02 05:36:07 +0300

    feat: create file api routes

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,9 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\FileController;
+use App\Http\Controllers\FolderController;
+use App\Http\Controllers\FileManagerController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +19,15 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+    Route::post('/files/upload', [FileController::class, 'upload']);
+    Route::get('/files/show/{id}', [FileController::class, 'show']);
+    Route::get('/files/index', [FileController::class, 'index']);
+    Route::put('/files/update/{id}', [FileController::class, 'update']);
+    Route::delete('/files/delete/{id}', [FileController::class, 'delete']);
+
+
+    
+
+
+
